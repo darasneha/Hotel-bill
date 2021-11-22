60e701e1d1ac01b6e581bc8ee6c518d9ea85df3a
darasneha <88605857+darasneha@users.noreply.github.com>
2021-11-22 06:32:56 -0800

    Add files via upload

---
+++ Electric Bill generation.py
@@ -0,0 +1,7 @@
+#Electric base program
+print("\t\t\t****Elecric Bill****")
+print("\t\t\t\tMenu\n1.current Bill\n2.Dish Bill\n3.Rent Bill")
+current = "500"
+Dish = "300"
+Rent = "200"
+print("current Bill = 500\nDish Bill = 300\nRent Bill = 200\n")
